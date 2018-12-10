7677936bee12417ab39f113f8e747b5a31081e9e
SuXY15 <suxy15@mails.tsinghua.edu.cn>
2018-12-10 20:33:12 +0800

    signalTester

--- src/signalTester.cpp
+++ src/signalTester.cpp
@@ -1,12 +1,19 @@
 #include "sigpack.h"
+#include <string>
 
 using namespace std;
 using namespace sp;
 
+inline int loadCSV(const char* fileName, vector<float> &time, vector<float> &data);
+
 int main()
 {
+    vector<float> time;
+    vector<float> data;
+    loadCSV("../data/data_1031_0399_tRGB.csv", time, data);
+
     // Number of samples
-    int N = 200;
+    int N = data.size();
 
     // Create a FIR filter and adaptive
     FIR_filt<double,double,double> G;
@@ -18,55 +25,42 @@ int main()
     int M = b.size();
 
     // Setup adaptive filter
-//  Ghat.setup_rls(M, 0.95, 50);
-//  Ghat.setup_nlms(M,0.5,0.001);
-    Ghat.setup_lms(M,0.05);
+    Ghat.setup_lms(M, 0.95);
 
     // Signal vectors
-    arma::vec x(N,arma::fill::randn);  // Input sig
-    arma::vec y(N,arma::fill::zeros);  // Model sig
-    arma::vec d(N,arma::fill::zeros);  // Output sig
-    arma::vec z(N,arma::fill::randn);  // Measurement noise
-    arma::vec e(N,arma::fill::zeros);  // Err sig
+    arma::vec x(N, arma::fill::zeros);  // Input sig
+    arma::vec y(N, arma::fill::zeros);  // Model sig
+    arma::vec d(N, arma::fill::zeros);  // Output sig
+    // arma::vec z(N, arma::fill::randn);  // Measurement noise
+    arma::vec e(N, arma::fill::zeros);  // Err sig
+
+    for(int i=0; i<N; i++) x[i] = data[i];
 
     // Log matrix
     arma::mat Wlog(M,N);
 
     // Apply G to input signal and add some measurement noise
-    d = G.filter(x)+0.0001*z;
-
-    // Filter - sample loop
-    for(int n=0;n<N;n++)
-    {
-        // Apply adaptiv filter
-        y(n) = Ghat(x(n));
-
-        // Calc error
-        e(n) = d(n)-y(n);
-
-        // Update filter
-//      Ghat.rls_adapt(e(n));
-//      Ghat.nlms_adapt(e(n));
-        Ghat.lms_adapt(e(n));
-
-        // Save to log
-        Wlog.col(n) = Ghat.get_coeffs();
-    }
+    d = G.filter(x);
 
     // Display result
     cout << "Filter coeffs:    " << b.t() << endl;
     cout << "Estimated coeffs: " << Ghat.get_coeffs().t() << endl;
-    gplot gp0,gp1;
+    gplot gp0;
     gp0.window("Plot", 10, 10, 500, 500);
-//    gp0.set_output("Wlog.png");
-    gp0.plot_add_mat(Wlog,"b");
+    gp0.plot_add(d, "b");
     gp0.plot_show();
+    return 0;
+}
 
-    gp1.window("Plot2", 600, 10, 500, 500);
-//    gp1.set_output("LMS_err.png");
-    arma::vec J = 10*log10(e%e);
-    gp1.plot_add(J,"|Error|^2");
-    gp1.plot_show();
-
-    return 1;
+inline int loadCSV(const char* fileName, vector<float> &time, vector<float> &data){
+    float t, R, G, B;
+    string line; char c;
+    ifstream file(fileName);
+    while(getline(file, line)){
+        stringstream strm(line);
+        strm >> t >> c >> R >> c >> G >> c >> B;
+        time.push_back(t);
+        data.push_back(G);
+    }
+    file.close();
 }
