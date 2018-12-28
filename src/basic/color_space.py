5339201fbe05ec7b04380dd394db1b7078e47d7b
YanHu <huyan15@mails.tsinghua.edu.cn>
2018-12-28 16:50:15 +0800

    Combination of color channels to fit ppg signal

--- src/basic/color_space.py
+++ src/basic/color_space.py
@@ -6,6 +6,7 @@ from sklearn import svm
 from sklearn import linear_model, tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import neural_network
+import copy
 
 path = r"D:\THU\curriculum\Software Engineering\Course_Project\data_1109_0427_RGB_fixed.txt"
 f = open(path, "r")
@@ -19,15 +20,16 @@ for line in f:
     
 #Smoothing
 length = len(channels[0])
+smoothed = copy.deepcopy(channels)
 for i in [2,3,4]:
     for j in range(length):
-        channels[i][j] = np.mean(channels[i][max(j - 5, 0):min(length - 1, j + 5)])
-    
+        smoothed[i][j] = np.mean(channels[i][max(j - 5, 0):min(length - 1, j + 5)])
+      
 #Specify each channel
 time = channels[0]
 index = np.arange(len(time))
 ppg = channels[1]
-colors = np.transpose(channels[3:])
+colors = np.transpose(smoothed[3:])
 
 #Split the training set and test set
 training_data = colors[:int(len(colors)*0.9)]
@@ -42,21 +44,22 @@ test_label = np.divide(np.subtract(test_label,50),200)
 #reg = linear_model.LinearRegression()
 reg =  linear_model.BayesianRidge()
 reg.fit(training_data, training_label)
-predicted_label = np.add(np.matmul(colors, reg.coef_), reg.intercept_)
+predicted_label = np.add(np.matmul(test_data, reg.coef_), reg.intercept_)
 
-plt.plot(index[:400], ppg[:400])
+
+plt.plot(index[:200], test_label[:200])
+plt.show()
+
+plt.plot(index[:200], np.transpose(test_data)[0][:200])
 plt.show()
 print(np.shape(training_label))
 
-plt.plot(index[:400], np.transpose(colors)[0][:400])
+plt.plot(index[:200], np.transpose(test_data)[1][:200])
 plt.show()
 print(np.shape(training_label))
 
-plt.plot(index[:400], np.transpose(colors)[1][:400])
-plt.show()
-print(np.shape(training_label))
 
-plt.plot(index[:400], predicted_label[:400])
+plt.plot(index[:200], predicted_label[:200])
 plt.show()
 print(np.shape(training_label))
 
